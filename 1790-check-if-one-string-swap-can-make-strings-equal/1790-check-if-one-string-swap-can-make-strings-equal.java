aea2b5806728a7f55b0df094c9740e01a8c90e60
Abhishek Singh <abhishek.1923cs1175@kiet.edu>
2022-03-03 18:46:44 +0530

    Time: 1 ms (79.45%), Space: 42.5 MB (19.39%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -3,7 +3,7 @@ class Solution {
     List<Integer> l = new ArrayList<>();
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) l.add(i);
-			if (l.size() > 2) return false; // added this line to short circuit the loop
+			if (l.size() > 2) return false;
         }
         return l.size() == 0 || (l.size() == 2 && s1.charAt(l.get(0)) == s2.charAt(l.get(1))&& s1.charAt(l.get(1)) == s2.charAt(l.get(0)));
     }
